1a9dc4f5bbe850e03bf708e5612d9439b12a7ed1
HP <HP@host.docker.internal>
2025-01-14 20:15:30 +0530

    initial commit

---
+++ Springpro/src/main/java/com/example/Springpro/Springpro/SpringproApplication.java
@@ -0,0 +1,14 @@
+package com.example.Springpro.Springpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringproApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hello .....!2025");
+		SpringApplication.run(SpringproApplication.class, args);
+	}
+
+}
